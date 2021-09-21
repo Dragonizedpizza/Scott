ee6acb19329e14eb97724342dbe183b36031be18
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-09-21 06:09:20 +0200

    update command type

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -1,11 +1,13 @@
-import { ApplicationCommandOptionData } from "discord.js";
+import { ApplicationCommandOptionData, Interaction, CommandInteractionOptionResolver, Message } from "discord.js";
 
 export interface Command {
     name: string;
     aliases: string[] | void;
     description: string;
     slash: {
-        options: ApplicationCommandOptionData[]
+        options: ApplicationCommandOptionData[],
+        run: ({ interaction, options }: { interaction: Interaction, options: CommandInteractionOptionResolver }) => Promise<Interaction | void> | void;
     },
     path: string;
+    run: ({ message, args }: { message: Message, args: string[] }) => Promise<Message | void> | void;
 };
