1e2d63475eba0ee33561b31d500e43526d6cd031
Dragonizedpizza <dragon@dragonizedpizza.com>
2021-09-21 06:04:21 +0200

    no bro

--- src/interfaces/Command.ts
+++ src/interfaces/Command.ts
@@ -2,8 +2,7 @@ import { ApplicationCommandOptionData } from "discord.js";
 
 export interface Command {
     name: string;
-    aliases: string | string[] | void;
-    alias: string | void;
+    aliases: string[] | void;
     description: string;
     slash: {
         options: ApplicationCommandOptionData[]
